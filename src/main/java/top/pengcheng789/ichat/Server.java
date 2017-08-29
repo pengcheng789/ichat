e4266095d9ee8a8b094ffd37ed77dda9ec9feee4
pengcheng789 <pengcheng789@foxmail.com>
2017-08-29 17:27:38 +0800

    重构构建工具

--- src/main/java/top/pengcheng789/ichat/Server.java
+++ src/main/java/top/pengcheng789/ichat/Server.java
@@ -7,9 +7,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.*;
-import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Set;
 
 /**
  *
@@ -36,46 +34,73 @@ public class Server {
     private void service(){
         ServerSocketChannel serverSocketChannel = getServerSocketChannel();
 
-        Selector selector = getSelector();
+//        Selector selector = getSelector();
+//        try {
+//            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+//        } catch (ClosedChannelException e) {
+//            LOGGER.error("Register server socket channel failure.");
+//            throw new RuntimeException(e);
+//        }
+
+        ByteBuffer receiveBuffer = ByteBuffer.allocate(1024*1024);
+        ByteBuffer sendBuffer = ByteBuffer.allocate(1024*1024);
+        sendBuffer.put("Connect success.".getBytes());
+        sendBuffer.flip();
+
+        SocketChannel socketChannel;
         try {
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
-        } catch (ClosedChannelException e) {
-            LOGGER.error("Register server socket channel failure.");
+            socketChannel = serverSocketChannel.accept();
+        } catch (IOException e) {
+            LOGGER.error("Accept socket failure.");
             throw new RuntimeException(e);
         }
 
-        ByteBuffer reciveBuffer = ByteBuffer.allocate(1024*1024);
-        ByteBuffer sendBuffer = ByteBuffer.allocate(1024*1024);
-
-        Runnable getSystemInputTask = () -> {
-            sendBuffer.clear();
-            sendBuffer.put(new Scanner(System.in).nextLine().getBytes());
-            sendBuffer.flip();
-        };
-        new Thread(getSystemInputTask).start();
+        try {
+            socketChannel.write(sendBuffer);
+        } catch (IOException e) {
+            LOGGER.error("Send ok failure.");
+            throw new RuntimeException(e);
+        }
 
         while (true) {
             try {
-                selector.select();
+                socketChannel.read(receiveBuffer);
+                receiveBuffer.flip();
+
+                StringBuilder message = new StringBuilder("");
+                while (receiveBuffer.hasRemaining()) {
+                   message.append((char)receiveBuffer.get());
+                }
+                LOGGER.info("Receive: " + message.toString());
+                //System.out.println("Receive: " + message.toString());
+                receiveBuffer.clear();
+
+                if (message.toString().equals("bye")) break;
             } catch (IOException e) {
-                LOGGER.error("Selector select failure.");
+                LOGGER.error("Receive message failure.");
                 throw new RuntimeException(e);
             }
 
-            Set<SelectionKey> keySet = selector.selectedKeys();
-            Iterator<SelectionKey> it = keySet.iterator();
-            while (it.hasNext()) {
-                SelectionKey key = it.next();
-                it.remove();
-
-                if (key.isAcceptable()) {
-                    accept(selector, serverSocketChannel);
-                } else if (key.isReadable()) {
-                    read(key, reciveBuffer);
-                } else if (key.isWritable()) {
-                    write(key, sendBuffer);
-                }
+            System.out.print("Type message: ");
+            sendBuffer.clear();
+            String sendMessage = new Scanner(System.in).nextLine();
+            sendBuffer.put(sendMessage.getBytes());
+            try {
+                sendBuffer.flip();
+                socketChannel.write(sendBuffer);
+                LOGGER.info("Send: " + sendMessage);
+            } catch (IOException e) {
+                LOGGER.error("Send message failure");
+                throw new RuntimeException(e);
             }
+        }
+
+        try {
+            serverSocketChannel.close();
+            LOGGER.info("Connection close.");
+        } catch (IOException e) {
+            LOGGER.error("Close server socket channel failure.");
+            throw new RuntimeException(e);
         }
 
     }
@@ -84,65 +109,12 @@ public class Server {
         try {
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.socket().bind(new InetSocketAddress(port));
-            serverSocketChannel.configureBlocking(false);
+            //serverSocketChannel.configureBlocking(false);
             LOGGER.info("Server is listening *:8090 ...");
             return serverSocketChannel;
         } catch (IOException e) {
             LOGGER.error("Create server socket channel failure.");
             throw new RuntimeException(e);
         }
-    }
-
-    private Selector getSelector() {
-        try {
-            return Selector.open();
-        } catch (IOException e) {
-            LOGGER.error("Create selector failure.");
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void accept(Selector selector, ServerSocketChannel serverSocketChannel) {
-        try {
-            SocketChannel socketChannel = serverSocketChannel.accept();
-            socketChannel.configureBlocking(false);
-            socketChannel.register(selector, SelectionKey.OP_READ);
-            LOGGER.info("Accept a socket from " + socketChannel.getRemoteAddress());
-        } catch (IOException e) {
-            LOGGER.error("Accept socket failure.");
-            throw new RuntimeException(e);
-        }
-    }
-
-    private void read(SelectionKey key, ByteBuffer receiveBuffer) {
-        SocketChannel socketChannel = (SocketChannel)key.channel();
-
-        receiveBuffer.clear();
-        try {
-            socketChannel.read(receiveBuffer);
-        } catch (IOException e) {
-            LOGGER.error("Receive data failure.");
-            throw new RuntimeException(e);
-        }
-
-        receiveBuffer.flip();
-        String message = new String(receiveBuffer.array());
-        LOGGER.info("Received: " + message);
-        System.out.println("Received: " + message);
-        key.interestOps(SelectionKey.OP_WRITE);
-    }
-
-    private void write(SelectionKey key, ByteBuffer sendBuffer) {
-        SocketChannel socketChannel = (SocketChannel) key.channel();
-        try {
-            socketChannel.write(sendBuffer);
-            String message = new String(sendBuffer.array());
-            LOGGER.info("Send: " + message);
-            System.out.println("Send: " + message);
-        } catch (IOException e) {
-            LOGGER.error("Send data failure.");
-            throw new RuntimeException(e);
-        }
-        key.interestOps(SelectionKey.OP_READ);
     }
 }
--- src/test/java/top/pengcheng789/ichat/Client.java
+++ src/test/java/top/pengcheng789/ichat/Client.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
@@ -11,18 +12,29 @@ public class Client {
             SocketChannel socketChannel = SocketChannel.open();
             socketChannel.connect(new InetSocketAddress("127.0.0.1", 8090));
 
-            ByteBuffer writeBuffer = ByteBuffer.allocate(32);
-            ByteBuffer readBuffer = ByteBuffer.allocate(32);
+            ByteBuffer receiveBuffer = ByteBuffer.allocate(32);
+            ByteBuffer sendBuffer = ByteBuffer.allocate(32);
 
-            writeBuffer.put("hello".getBytes());
-            writeBuffer.flip();
+            String sendMessage = "";
+            while (!sendMessage.equals("bye")) {
+                socketChannel.read(receiveBuffer);
+                receiveBuffer.flip();
+                System.out.print("Receive message: ");
+                while (receiveBuffer.hasRemaining()) {
+                    System.out.print((char)receiveBuffer.get());
+                }
+                System.out.println();
+                receiveBuffer.clear();
 
-            while (true) {
-                writeBuffer.rewind();
-                socketChannel.write(writeBuffer);
-                readBuffer.clear();
-                socketChannel.read(readBuffer);
+                System.out.print("Type message: ");
+                sendMessage = new Scanner(System.in).nextLine();
+                sendBuffer.clear();
+                sendBuffer.put(sendMessage.getBytes());
+                sendBuffer.flip();
+                socketChannel.write(sendBuffer);
+                //System.out.println("Send message: " + sendMessage);
             }
+            socketChannel.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
